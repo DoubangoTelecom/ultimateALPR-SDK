f2f67b0af283880d1681580a4715ac62a6a42667
Doubango Telecom <github@doubango.org>
2021-11-03 23:27:00 +0100

    Disable IENV by default (Python command)

--- binaries/windows/x86_64/python_recognizer.bat
+++ binaries/windows/x86_64/python_recognizer.bat
@@ -1,5 +1,5 @@
 setlocal
 set PYTHONPATH=%PYTHONPATH%;.;../../../python
 set PATH=%PATH%;%~dp0
-python ../../../samples/python/recognizer/recognizer.py --image ../../../assets/images/lic_us_1280x720.jpg --assets ../../../assets --ienv_enabled True --klass_lpci_enabled True --klass_vcr_enabled True --klass_vmmr_enabled True --klass_vbsr_enabled True
+python ../../../samples/python/recognizer/recognizer.py --image ../../../assets/images/lic_us_1280x720.jpg --assets ../../../assets --ienv_enabled False --klass_lpci_enabled True --klass_vcr_enabled True --klass_vmmr_enabled True --klass_vbsr_enabled True
 endlocal
